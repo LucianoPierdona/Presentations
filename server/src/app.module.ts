8ef65a1ba885c6d440a04b632ed209251d40a0c0
LucianoPierdona <lucianompj1@gmail.com>
2022-07-08 22:14:30 -0300

    add `config` module and service. implement `Docker` and `Typeorm`

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,41 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoggerModule } from 'nestjs-pino';
+import { join } from 'path';
 import { AttendeeModule } from './attendee/attendee.module';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
 import { PresentationModule } from './presentation/presentation.module';
 
 @Module({
-  imports: [LoggerModule.forRoot(), PresentationModule, AttendeeModule],
+  imports: [
+    LoggerModule.forRoot(),
+    PresentationModule,
+    AttendeeModule,
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        name: configService.getString('DB_CONNECTION_NAME'),
+        type: 'postgres',
+        host: configService.getString('DB_CONNECTION_HOST'),
+        port: configService.getNumber('DB_CONNECTION_PORT'),
+        username: configService.getString('DB_CONNECTION_USERNAME'),
+        password: configService.getString('DB_CONNECTION_PASSWORD'),
+        database: configService.getString('DB_CONNECTION_DATABASE'),
+        entities: [join(__dirname, '../') + '**/*.entity.js'],
+        dropSchema: configService.getBoolean('DB_CONNECTION_DROP_SCHEMA'),
+        synchronize: configService.getBoolean('DB_CONNECTION_SYNCHRONIZE'),
+        logging: configService.getBoolean('DB_CONNECTION_LOGGING'),
+        cache: configService.getBoolean('DB_CONNECTION_CACHE'),
+        migrations: [
+          join(__dirname, '../', 'database', 'migrations') + '**/*.js',
+        ],
+        migrationsRun: false,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
